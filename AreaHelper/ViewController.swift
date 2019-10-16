9012b4dfce4ef97027a3df31e4deab2993cbd5d1
Jeewoo Lee <jlee@lcs.on.ca>
2019-10-16 19:57:13 -0400

    Initial Commit

---
+++ AreaHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AreaHelper
+//
+//  Created by Lee, Jeewoo on 2019-10-16.
+//  Copyright © 2019 Lee, Jeewoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
